4a056893623df1105960fbc04d76a62af12df1d8
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-12 02:03:36 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <div>
+      <div className="hero">
+        <h1 className="title">Next.js + Tailwind CSS</h1>
+      </div>
+    </div>
+  )
+};
+
+export default Index;

dd35d7c83230824e636d224fe79d401e271996cf
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-14 10:59:55 +0300

    redux setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/index.css'
+import { Provider } from 'react-redux'
+import { store } from '../store'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp;

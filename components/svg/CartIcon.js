7136aa02533bd3f19c321c7c6c3e9c8079e130c2
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-19 07:02:14 +0300

    added api-sample and svg-components

---
+++ components/svg/CartIcon.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function SvgIconCart(props) {
+  return (
+    <svg viewBox="0 0 24 24" width="100%" height="100%" {...props}>
+      <path
+        className="icon-cart_svg__heroicon-ui"
+        d="M17 16a3 3 0 11-2.83 2H9.83a3 3 0 11-5.62-.1A3 3 0 015 12V4H3a1 1 0 110-2h3a1 1 0 011 1v1h14a1 1 0 01.9 1.45l-4 8a1 1 0 01-.9.55H5a1 1 0 000 2h12zM7 12h9.38l3-6H7v6zm0 8a1 1 0 100-2 1 1 0 000 2zm10 0a1 1 0 100-2 1 1 0 000 2z"
+      />
+    </svg>
+  );
+}
+
+export default SvgIconCart;

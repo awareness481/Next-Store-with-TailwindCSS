8f3af15a9420c38a096cd495e6f54fcf5e4c9735
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-19 07:00:59 +0300

    small styling changes in Banner, added Cart in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,14 @@ import React from 'react';
 import SvgVercel from './svg/Vercel';
 import styles from './Navbar.module.css';
 
+import CartContainer from './CartContainer';
+
 const Navbar = () => {
   return (
     <nav className={`${styles.navbar} w-full h-32 flex flex-row justify-between items-center`}>
       <SvgVercel  className='ml-16'/>
       <div className='navbar__cart pr-16'>
-        <span className='navbar__cart--display'>Cart</span>
+        <span className='navbar__cart--display'><CartContainer /></span>
       </div>
     </nav>
   );

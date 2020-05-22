3b6a8311c16a04c3cc944f39df189b9e3a24357f
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-22 05:49:31 +0300

    improved checkboxes

--- components/Filters.js
+++ components/Filters.js
@@ -3,37 +3,53 @@ import { connect } from "react-redux";
 
 const Filters = ({ dispatch }) => {
   const handleSelect = (e) => {
-
     dispatch({
-      type: 'TOGGLE_FILTER',
+      type: "TOGGLE_FILTER",
       filter: {
         id: e.target.id,
-        checked: e.target.checked
-      }
-    })
-  }
+        checked: e.target.checked,
+      },
+    });
+  };
 
   return (
     <div className="filters flex-grow">
       <form>
-        <fieldset className="flex flex-col">
-          <legend>Filters</legend>
+        <fieldset className="flex flex-col mt-8 pl-8">
+          <legend className='text-3xl mb-4'>Brands</legend>
 
-          <div>
-            <input type="checkbox" id="Lenovo" name="brand" onClick={handleSelect} />
-            <label htmlFor="Lenovo">Lenovo</label>
+          <div className='pl-4'>
+            <input
+              type="checkbox"
+              id="Lenovo"
+              name="brand"
+              onClick={handleSelect}
+              className="form-checkbox h-8 w-8 mr-4"
+            />
+            <label htmlFor="Lenovo" className='text-2xl align-middle'>Lenovo</label>
           </div>
 
-          <div>
-            <input type="checkbox" id="HP" name="brand" onClick={handleSelect}/>
-            <label htmlFor="HP">HP</label>
+          <div className='pl-4'>
+            <input
+              type="checkbox"
+              id="HP"
+              name="brand"
+              onClick={handleSelect}
+              className="form-checkbox h-8 w-8 mr-4"
+            />
+            <label htmlFor="HP" className='text-2xl align-middle'>HP</label>
           </div>
 
-          <div>
-            <input type="checkbox" id="Dell" name="brand" onClick={handleSelect}/>
-            <label htmlFor="Dell">Dell</label>
+          <div className='pl-4'>
+            <input
+              type="checkbox"
+              id="Dell"
+              name="brand"
+              onClick={handleSelect}
+              className="form-checkbox h-8 w-8 mr-4"
+            />
+            <label htmlFor="Dell" className='text-2xl align-middle'>Dell</label>
           </div>
-
         </fieldset>
       </form>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,6 @@ const Index = () => {
       <Banner />
       <SearchContainer />
       <ResultsContainer />
-      <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
-      </div>
     </div>
   )
 };

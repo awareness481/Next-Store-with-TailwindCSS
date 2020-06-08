71899594d9771e3ce4db5784253d6e34a4772c34
Jesse Jafa <jesse.jafa@gmail.com>
2020-06-08 07:17:45 +0300

    color improvements

--- components/Laptop.js
+++ components/Laptop.js
@@ -145,7 +145,7 @@ const Laptop = ({ laptop }) => {
               <span className="title-font font-medium text-2xl text-gray-900">
                 $ {laptop.price}
               </span>
-              <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+              <button className="flex ml-auto text-white bg-red-300 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
                 Button
               </button>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
--- components/SearchContainer.js
+++ components/SearchContainer.js
@@ -3,15 +3,15 @@ import style from "./SearchContainer.module.css";
 
 const SearchContainer = () => {
   return (
-    <div className="w-full flex flex-row">
+    <div className="w-full flex flex-row bg-warm-grey-050">
       <div className={`${style.filters} flex-grow
        h-16`}>
-        <h3 className='text-center text-3xl'>Filters</h3>
+        <h3 className='text-center text-3xl text-yellow-vivid-600'>Filters</h3>
       </div>
       <div
         className={`${style.search} ml-auto h-16 flex flex-row items-center`}
       >
-        <h2 className={`${style.search__title} text-center text-3xl`}>
+        <h2 className={`${style.search__title} text-center text-3xl text-yellow-vivid-600`}>
           Find your next laptop
         </h2>
       </div>

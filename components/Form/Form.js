f6ed3427f01576c7ad68a9e225834ed06e7fe22b
Jesse Jafa <jesse.jafa@gmail.com>
2020-07-21 09:25:12 +0300

    changed from multipage form to 2step

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
-import Names from "./Names";
-import Address from "./Address";
-import Contact from "./Contact";
-import Review from "./Review";
-import Submit from "./Submit;";
+import FormStructure from './FormStructure';
 
-const steps = ["names", "address", "contact", "review", "submit"];
 
 const defaultData = {
   firtName: "",
@@ -17,67 +12,15 @@ const defaultData = {
   phone: "",
 };
 
-const CheckoutForm = ({ items }) => {
-  const [formData, setFormData] = useState(defaultData);
-  const [step, setStep] = useState(0);
-
-  if (steps[step] === "names")
-    return (
-      <Names
-        incrementStep={setStep}
-        step={step}
-        setData={setFormData}
-        data={formData}
-      />
-    );
-  else if (steps[step] === "address")
-    return (
-      <Address
-        incrementStep={setStep}
-        step={step}
-        setData={setFormData}
-        data={formData}
-      />
-    );
-  else if (steps[step] === "contact")
-    return (
-      <Contact
-        incrementStep={setStep}
-        step={step}
-        setData={setFormData}
-        data={formData}
-      />
-    );
-  else if (steps[step] === "review")
-    return (
-      <Review
-        incrementStep={setStep}
-        step={step}
-        setData={setFormData}
-        data={formData}
-      />
-    );
-  else if (steps[step] === "submit")
-    return (
-      <Submit
-        incrementStep={setStep}
-        step={step}
-        setData={setFormData}
-        data={formData}
-      />
-    );
-};
 
 const Form = ({ items }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form done!");
-    return;
-  };
+  const [data, setData] = useState({...defaultData})
 
   return (
-    <form className={`checkout`} onSubmit={handleSubmit}>
-      <Form items={items} />
-    </form>
+    <>
+      <FormStructure setData={setData} />
+    </>
   );
 };
+
+export default Form;

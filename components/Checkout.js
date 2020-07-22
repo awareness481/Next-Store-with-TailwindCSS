8b72fdd3dad6ae2520c3fdf706224f5f8f42fdc2
Jesse Jafa <jesse.jafa@gmail.com>
2020-07-22 11:17:43 +0300

    mobile styling fixes for Checkout

--- components/Checkout.js
+++ components/Checkout.js
@@ -7,16 +7,16 @@ const Checkout = ({ cart, dispatch }) => {
   console.log(cart);
   const mapCartItems = () => {
     return Object.keys(cart).map((item) => (
-      <div className="item flex flex-row w-1/2 mx-auto my-0 justify-center">
+      <div className="item flex flex-row w-full sm:w-1/2 mx-auto sm:my-0 sm:justify-center">
         <img
           src={cart[item].image}
-          className={`item__img`}
+          className={`item__img ml-0`}
           width="100"
           height="100"
         />
-        <div className="self-center px-16">
-          <h2 className="text-2xl">{cart[item].name}</h2>
-          <span className="text-xl">${cart[item].price}</span>
+        <div className="self-center px-2 flex flex-row sm:flex-col sm:px-16">
+          <h2 className="text-2xl pr-8 sm:pr-0">{cart[item].name}</h2>
+          <span className="text-xl self-center">${cart[item].price}</span>
         </div>
         <button
           onClick={() =>
@@ -25,6 +25,7 @@ const Checkout = ({ cart, dispatch }) => {
               item,
             })
           }
+          className="ml-auto mr-0 sm:mx-0"
         >
           Remove
         </button>
@@ -34,9 +35,9 @@ const Checkout = ({ cart, dispatch }) => {
 
   return (
     <div className={`flex flex-col w-4/5 mx-auto my-0`}>
-      {Object.keys(cart).length === 0 ? "No saved items" : mapCartItems()}
+      {Object.keys(cart).length === 0 ? "No saved items" : (<div className='mt-32'>{mapCartItems()}</div>)}
       <Link href='/form-test'>
-        <button className='mx-auto w-32 rounded-md px-8 py-3 text-2xl bg-yellow-vivid-500 text-white'>Pay</button>
+        <button className='mx-auto w-32 rounded-md px-8 py-3 mt-8 text-2xl bg-yellow-vivid-500 text-white'>Pay</button>
       </Link>
     </div>
   );

99d790adcfddaef014895ae959eea29116628bbb
Jesse Jafa <jesse.jafa@gmail.com>
2020-07-22 08:49:31 +0300

    improved laptopdescription styling

--- components/LaptopDescription.js
+++ components/LaptopDescription.js
@@ -14,10 +14,10 @@ const LaptopDescription = ({items}) => {
   
 
   return (
-    <div className={`${style.description} w-9/10 md:w-3/5 flex flex-col justify-center`}>
+    <div className={`${style.description} w-9/10 md:w-2/5 flex flex-col justify-center py-6`}>
       {
         mapItems().map((item) => (
-          <div className={`w-full flex flex-row py-4 px-2 justify-center text-2xl`}>
+          <div className={`w-full flex flex-row py-4 px-12 justify-center text-2xl`}>
             <span className='type mx-auto flex-grow'>{item[0]}</span>
             <span className={`${style.value} w-9/10 block text-left`}>{item[1]}</span>
           </div>

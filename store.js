79f243334f7fdd9e008dec3cda611d4425022d57
Jesse Jafa <jesse.jafa@gmail.com>
2020-07-26 08:52:16 +0300

    removing devtools on production test

--- store.js
+++ store.js
@@ -1,25 +1,29 @@
-import { createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { useMemo } from 'react';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { useMemo } from "react";
 
-import rootReducer from './reducers/index';
+import rootReducer from "./reducers/index";
 
 let store;
-
-export const initStore = (state = {
-  cart: {},
-  results: [],
-  filters: []
-}) => {
+console.log(process.env);
+export const initStore = (
+  state = {
+    cart: {},
+    results: [],
+    filters: [],
+  }
+) => {
   return createStore(
     rootReducer,
     state,
-    composeWithDevTools(applyMiddleware())
-  )
+    process.env.NODE_ENV === "development"
+      ? composeWithDevTools(applyMiddleware())
+      : applyMiddleware()
+  );
 };
 
 export const initializeStore = (preloadedState) => {
-  let _store = store ?? initStore(preloadedState)
+  let _store = store ?? initStore(preloadedState);
 
   // After navigating to a page with an initial Redux state, merge that state
   // with the current state in the store, and create a new store
@@ -27,20 +31,20 @@ export const initializeStore = (preloadedState) => {
     _store = initStore({
       ...store.getState(),
       ...preloadedState,
-    })
+    });
     // Reset the current store
-    store = undefined
+    store = undefined;
   }
 
   // For SSG and SSR always create a new store
-  if (typeof window === 'undefined') return _store
+  if (typeof window === "undefined") return _store;
   // Create the store once in the client
-  if (!store) store = _store
+  if (!store) store = _store;
 
-  return _store
-}
+  return _store;
+};
 
 export function useStore(initialState) {
-  const store = useMemo(() => initializeStore(initialState), [initialState])
-  return store
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
 }
